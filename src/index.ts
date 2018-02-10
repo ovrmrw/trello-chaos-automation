67f50cef06b74c468b5ef8e3ae96111a41547b86
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2018-02-11 01:55:51 +0900

    feat: can calculate metrix

--- src/index.ts
+++ src/index.ts
@@ -23,31 +23,55 @@ async function main() {
       list: card._listName,
       card: card.name,
       daysFromLastActivity: card._daysFromLastActivity,
-      listOrder: lists.find(l => l.id === card.idList)!.pos,
+      listOrder: lists.find(list => list.id === card.idList)!.pos,
       cardOrder: card.pos,
     }));
   console.log('ROTTEN CARDS:', tapWriteFileSync('rotten_cards.json', orderBy(rottenCards, ['listOrder', 'cardOrder'], ['asc', 'asc'])));
-  slackClient.postMessage(orderBy(rottenCards, ['listOrder', 'cardOrder'], ['asc', 'asc']).map(card => {
+  await slackClient.postMessage(orderBy(rottenCards, ['listOrder', 'cardOrder'], ['asc', 'asc']).map(card => {
     delete card.listOrder;
     delete card.cardOrder;
     return card;
-  }), '一定期間Activityのないカード');
+  }), '一定期間アクティビティのないカード');
 
-  const releaseLists = cards
+  const incompleteReleaseCards = cards
     .filter(card => card._listName.includes('RELEASE') && card._releaseStatus !== 'OK')
     .map(card => ({
       list: card._listName,
       card: card.name,
       releaseStatus: card._releaseStatus,
-      listOrder: lists.find(l => l.id === card.idList)!.pos,
+      listOrder: lists.find(list => list.id === card.idList)!.pos,
       cardOrder: card.pos,
     }));
-  console.log('RELEASE LISTS:', tapWriteFileSync('release_lists.json', orderBy(releaseLists, ['listOrder', 'cardOrder'], ['asc', 'asc'])));
-  slackClient.postMessage(orderBy(releaseLists, ['listOrder', 'cardOrder'], ['asc', 'asc']).map(list => {
-    delete list.listOrder;
-    delete list.cardOrder;
-    return list;
+  console.log('IMCOMPLETE RELEASE CARDS:', tapWriteFileSync('incomplete_release_cards.json', orderBy(incompleteReleaseCards, ['listOrder', 'cardOrder'], ['asc', 'asc'])));
+  await slackClient.postMessage(orderBy(incompleteReleaseCards, ['listOrder', 'cardOrder'], ['asc', 'asc']).map(card => {
+    delete card.listOrder;
+    delete card.cardOrder;
+    return card;
   }), 'リリースリストの完了条件を満たしていないカード');
+
+  if (incompleteReleaseCards.length === 0) {
+    const releaseCards = cards
+      .filter(card => card._listName.includes('RELEASE'))
+      .map(card => ({
+        list: card._listName,
+        sp: card._storyPoint,
+        leadtime: card._leadtime,
+      }))
+      .reduce((p, card) => {
+        if (!p[card.list]) {
+          p[card.list] = { cards: 0, sp_sum: 0, leadtime_sum: 0, leadtime_ave: 0 };
+        }
+        p[card.list].cards += 1;
+        p[card.list].sp_sum += card.sp;
+        p[card.list].leadtime_sum += card.leadtime;
+        return p;
+      }, {} as any);
+    Object.entries(releaseCards).forEach(([key, obj]) => {
+      releaseCards[key].leadtime_ave = obj.leadtime_sum / obj.cards;
+    });
+    console.log('RELEASE CARDS:', tapWriteFileSync('release_cards_metrix.json', releaseCards));
+    await slackClient.postMessage(releaseCards, 'リリースリストのメトリクス');
+  }
 }
 
 // entry point
--- src/slack-client.ts
+++ src/slack-client.ts
@@ -4,14 +4,18 @@ import { slack } from './config';
 export class SlackClient {
   private readonly webhookUrl: string = slack.webhookUrl;
 
+  constructor(private disabled: boolean = false) { }
+
   postMessage(message: string | any, title?: string): Promise<void> {
     const _title = title ? `**【${title}】**\n` : '';
     const body = typeof message === 'string'
       ? { text: _title + message }
       : { text: _title + JSON.stringify(message, null, 4) };
-    return axios.post(this.webhookUrl, body)
-      .then(res => {
-        console.log('SlackClient#postMessage:', res.data);
-      });
+    return !this.disabled
+      ? axios.post(this.webhookUrl, body)
+        .then(res => {
+          console.log('SlackClient#postMessage:', res.data);
+        })
+      : Promise.resolve();
   }
 }
